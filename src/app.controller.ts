5a95a655d74845e4072e657d413fabd9bac4ebd4
Adroaldo <hygino@msn.com>
2022-07-19 19:21:04 -0300

    Nest CLI

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,14 @@
-import { Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+
 import { Request } from 'express';
 
-@Controller('/user')
-export class AppController {
-  @Get()
-  getUsers() {
-    return { name: 'Dilma', email: 'dilma@gmail.com' };
-  }
-
-  @Post()
-  store(@Req() req: Request) {
-    //console.log(req.body);
-    return req.body;
-  }
-
-  @Patch('/:userId')
-  update(@Req() req: Request) {
-    //console.log(req.body);
-    return req.body;
-  }
-
-  @Get('/:userId')
-  getUser(@Param() params: { userId: number }) {
-    return params;
-  }
-
-  @Delete('/:userId')
-  deleteUser(@Param() params: { userId: number }) {
-    return params;
-  }
-}
+@Controller()
+export class AppController {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('user')
+export class UserController {
+  @Get()
+  getUsers() {
+    return { name: 'Dilma', email: 'dilma@gmail.com' };
+  }
+
+  @Post()
+  store(@Req() req: Request) {
+    //console.log(req.body);
+    return req.body;
+  }
+
+  @Patch('/:userId')
+  update(@Req() req: Request) {
+    //console.log(req.body);
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params;
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params;
+  }
+}
