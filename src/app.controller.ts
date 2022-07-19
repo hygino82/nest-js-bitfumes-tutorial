4dca8191894dfdfdb9ef601115525e565835444c
Adroaldo <hygino@msn.com>
2022-07-19 12:35:38 -0300

    Nest controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
-@Controller()
+@Controller('/user')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers() {
+    return { name: 'Dilma', email: 'dilma@gmail.com' };
+  }
+
+  @Post()
+  store(@Req() req: Request) {
+    //console.log(req.body);
+    return req.body;
+  }
+
+  @Patch('/:userId')
+  update(@Req() req: Request) {
+    //console.log(req.body);
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params;
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params;
   }
 }
