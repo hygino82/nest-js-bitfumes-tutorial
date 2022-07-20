7ae1842c1ded12a63e3f506a08bb14902f20c0dd
Adroaldo <hygino@msn.com>
2022-07-20 07:58:14 -0300

    Testando uma lista

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,11 +9,19 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 
+const userList = [
+  { name: 'Dilma', email: 'dilma@gmail.com', id: 1 },
+  { name: 'Gorete', email: 'gorete@gmail.com', id: 2 },
+  { name: 'Godofredo', email: 'godofredo@gmail.com', id: 3 },
+  { name: 'Tenorio', email: 'tenorio@gmail.com', id: 4 },
+  { name: 'Jupira', email: 'jupira@gmail.com', id: 5 },
+];
+
 @Controller('user')
 export class UserController {
   @Get()
   getUsers() {
-    return { name: 'Dilma', email: 'dilma@gmail.com' };
+    return userList;
   }
 
   @Post()
@@ -30,7 +38,9 @@ export class UserController {
 
   @Get('/:userId')
   getUser(@Param() params: { userId: number }) {
-    return params;
+    const user = userList.filter((x) => x.id == params.userId);
+    if (user.length === 0) return 'Não encontrado';
+    return user;
   }
 
   @Delete('/:userId')
